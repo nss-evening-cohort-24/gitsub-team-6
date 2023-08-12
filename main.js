827e5775f8956b975b75688e67878abf9b2f3ea7
Johnny Saniat <Jcsaniat@gmail.com>
2023-08-12 14:11:06 -0500

    recreated the star and the branch buttons, added interpolation, formatted, and removed card image placeholder

--- main.js
+++ main.js
@@ -265,15 +265,20 @@ const pinsOnDom = (array) => {
   for(const pinnedRepo of array) {
     domString += `
     <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-display" viewBox="0 0 16 16">
+    <path d="M0 4s0-2 2-2h12s2 0 2 2v6s0 2-2 2h-4c0 .667.083 1.167.25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75c.167-.333.25-.833.25-1.5H2s-2 0-2-2V4zm1.398-.855a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3H2c-.325 0-.502.078-.602.145z"/>
+  </svg>
   <div class="card-body">
     <h5 class="card-title">${pinnedRepo.name}</h5>
     <p class="card-text">${pinnedRepo.description}</p>
     <p class="card-text">${pinnedRepo.tags}</p>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" id="fav-btn" viewBox="0 0 16 16">
   <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+  <p>${pinnedRepo.starred}</p>
 </svg>
-<i class="fas fa-code-branch" id="branch-btn"></i>
+<i class="fas fa-code-branch" id="branch-btn"> 
+<p> ${pinnedRepo.branchNum} </p> 
+</i>
   </div>
 </div>`
   }
